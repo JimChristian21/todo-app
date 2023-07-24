0d56f84024b7f15db1d4dda3e047b7fadfeb376e
Jim Christian Edullantes <jimchristianedullantes9@gmail.com>
2023-07-24 23:13:24 +0800

    Added statis for the app

--- src/App.js
+++ src/App.js
@@ -3,8 +3,33 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
+      <header className="App-header bg-black">
+        <div className="container p-5 bg-blue-500">
+          <div>
+            <label className="">Todo: </label>
+            <input 
+              type="text" 
+              className="pl-2 p-2 border-2 text-base rounded border-black"
+            />
+            <button 
+              type="button" 
+              className="ml-6 p-2 border-2 text-base rounded border-black"
+              >
+                Add
+            </button>
+          </div>
+
+          <hr className="mt-5"/>
+          
+          <div>
+            <div>
+              <input type="checkbox"/>
+              <label className="text-base">Run</label>
+              <button type="button" className="text-base">Edit</button>
+              <button type="button" className="text-base">Delete</button>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
